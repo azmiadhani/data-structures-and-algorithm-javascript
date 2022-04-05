33f6d1d62ed4430c05a8867c18626f893a24a68f
Azmi Adhani <azmiadhani@gmail.com>
2022-04-06 07:21:24 +0800

    Exercise: Big O calculation answer

--- 1-Big O/3.0-exercise-big-o-calculation.js
+++ 1-Big O/3.0-exercise-big-o-calculation.js
@@ -6,9 +6,15 @@ function funChallenge(input) {
   // O(n)
   for (let i = 0; i < input.length; i++) {
     anotherFunction(); // O(n)
-    let stranger = true; //O(1)
-    a++; // O(1)
+    let stranger = true; // O(1) => O(n) because it depends on the size of the input
+    a++; // O(1) => O(n) because it depends on the size of the input
   }
   return a; // O(1)
 }
+// My answer :
 // Big O of the above function is O(n), because it has a loop, but still not sure if its correct, because it's not clear if it's O(n) because its got the O(1) and some other function that is undefined (can't be seen)
+
+// Answer to the above question:
+// Big O(3+4n)
+// Actually, you won't need to calculate Big O step by step like here. Soon you will see an easier way to do this that is more practical.
+// Actually this function is Big O(n), we gonna go through that later on
