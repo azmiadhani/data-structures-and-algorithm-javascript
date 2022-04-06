ea4c78c51c8002d7f7da28b77dcef3b1f42f894b
Azmi Adhani <azmiadhani@gmail.com>
2022-04-06 14:52:16 +0800

    rule 3 addition

--- 1-Big O/5.2-rule-book-different-terms-for-inputs.js
+++ 1-Big O/5.2-rule-book-different-terms-for-inputs.js
@@ -11,3 +11,4 @@ const compressTwoBoxes = (boxes, boxes2) => {
 // when there's two different inputs (parameter in this case)
 // then for the big O :
 // O(a + b)
+// when you see loops one after another, then for notation you'll use addition (+)
