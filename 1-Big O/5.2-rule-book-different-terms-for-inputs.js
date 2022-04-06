f94632b8503d10e66eb23a912156e7f556ef9c11
Azmi Adhani <azmiadhani@gmail.com>
2022-04-06 14:23:37 +0800

    Rule 3: different terms for inputs

---
+++ 1-Big O/5.2-rule-book-different-terms-for-inputs.js
@@ -0,0 +1,13 @@
+const compressTwoBoxes = (boxes, boxes2) => {
+  boxes.forEach((box) => {
+    console.log(box);
+  });
+
+  boxes2.forEach((box) => {
+    console.log(box);
+  });
+};
+
+// when there's two different inputs (parameter in this case)
+// then for the big O :
+// O(a + b)
